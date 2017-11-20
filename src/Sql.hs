e44c760f893bc730b361acfb4f186fd6807b5789
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-11-20 11:51:16 +0000

    added column labels in select

--- src/Sql.hs
+++ src/Sql.hs
@@ -4,21 +4,37 @@ import Data.Text
 import Text.Parsec
 
 data Expr = Number Int
+          | Col Text
   deriving (Eq, Show)
 
-data Sql = Select Expr
+data Sql = Select [ Expr ]
   deriving (Eq, Show)
 
 parseSQL :: Text -> Either Text Sql
 parseSQL text =
-  case parse monParser "" text
+  case parse sql "" text
   of
     Left error   -> Left . pack $ show error
     Right result -> Right result
-  where monParser = do
+  where sql = selectClause
+
+        selectClause = do
           string "SELECT"
           spaces
-          n <- integer
-          return (Select $ Number n)
+          exprs <- expressionList
 
+          return (Select exprs)
+
+        expressionList = expression `sepBy1` comma
+
+        expression =  (Number <$> integer)
+                  <|> (Col <$> columnName)
+          
+        comma = spaces >> string ","  >> spaces
+
+        columnName = do
+          l  <- letter
+          ls <- many alphaNum
+          return $ pack $ l:ls
+          
         integer = fmap read (many1 digit)
--- test/ConsoleSpec.hs
+++ test/ConsoleSpec.hs
@@ -8,10 +8,14 @@ spec :: Spec
 spec = describe "SQL Mini Interpreter" $ do
   it "interprets '.exit' as Exit command" $ do
     interpret ".exit" `shouldBe` Exit
+    
   it "interprets 'SELECT 42' as an SqlStatement" $ do
-    interpret "SELECT 42" `shouldBe` SqlStatement (Select (Number 42))
+    interpret "SELECT 42" `shouldBe` SqlStatement (Select [ Number 42 ])
   it "interprets 'SELECT 1' as an SqlStatement" $ do
-    interpret "SELECT 1" `shouldBe` SqlStatement (Select (Number 1))
+    interpret "SELECT 1" `shouldBe` SqlStatement (Select [ Number 1 ])
 
+  it "interprets 'SELECT Foo, Bar' as a SqlStatement" $ do
+    interpret "SELECT Foo, Bar" `shouldBe` SqlStatement (Select [ Col "Foo", Col "Bar"] )
+    
   it "interprets unknown string  as Unknown command" $ do
     interpret "foo" `shouldBe` Unknown "(line 1, column 1):\nunexpected \"f\"\nexpecting \"SELECT\""
