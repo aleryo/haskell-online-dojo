613d3b87b46f964d30ae1d8157d717e75de74550
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-11-13 12:52:37 +0100

    tests pass

--- test/KataPotterSpec.hs
+++ test/KataPotterSpec.hs
@@ -4,12 +4,14 @@ import           Test.Hspec
 
 spec :: Spec
 spec = describe "Kata Potter" $ do
---   2 copies of the first book
--- 2 copies of the second book
--- 2 copies of the third book
--- 1 copy of the fourth book
--- 1 copy of the fifth book
-  --
+  --   2 copies of the first book
+  -- 2 copies of the second book
+  -- 2 copies of the third book
+  -- 1 copy of the fourth book
+  -- 1 copy of the fifth book
+    --
+    -- quel  est le prix ?
+    -- il y a des reductions pour 2 vol -> 5 %, 3 vol -> 10 %, 4 vol -> 20 %, 5 vol -> 25 %
   it "pass the test" $
     True `shouldBe` True
 
