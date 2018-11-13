7bf6a4ae4fbb4a45c17d8c7c3d3413db3a5718b8
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-11-13 12:51:02 +0100

    tests pass

--- test/KataPotterSpec.hs
+++ test/KataPotterSpec.hs
@@ -4,5 +4,12 @@ import           Test.Hspec
 
 spec :: Spec
 spec = describe "Kata Potter" $ do
+--   2 copies of the first book
+-- 2 copies of the second book
+-- 2 copies of the third book
+-- 1 copy of the fourth book
+-- 1 copy of the fifth book
+  --
   it "pass the test" $
     True `shouldBe` True
+
