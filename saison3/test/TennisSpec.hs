854ce834b03116c996e530ba242cd8db7aff52eb
Arnaud Bailly <arnaud.oqube@gmail.com>
2019-09-17 13:34:41 +0200

    score pour 2 coups de 2

--- saison3/test/TennisSpec.hs
+++ saison3/test/TennisSpec.hs
@@ -2,26 +2,37 @@ module TennisSpec where
 
 import Test.Hspec
 
-data PartieDeTennis = Initial | AMarque
+data PartieDeTennis = Initial 
+                    | BMarque
+                    | AMarque { resteDeLaPartie :: PartieDeTennis }
 
-data Point = Zero | Quinze
+data Point = Zero | Quinze | Trente
     deriving (Eq, Show)
 
 initial :: PartieDeTennis
 initial = Initial
 
-aMarque :: PartieDeTennis
-aMarque = AMarque
+aMarque :: PartieDeTennis -> PartieDeTennis
+aMarque = AMarque 
+
+bMarque :: PartieDeTennis
+bMarque = BMarque
 
 type Score = (Point, Point)
 
 scoreDeTennis :: PartieDeTennis -> Score
-scoreDeTennis AMarque = (Quinze, Zero)
-scoreDeTennis _ = (Zero, Zero)
+scoreDeTennis (AMarque (AMarque _)) = (Trente, Zero)
+scoreDeTennis (AMarque _)           = (Quinze, Zero)
+scoreDeTennis BMarque               = (Zero, Quinze)
+scoreDeTennis _                     = (Zero, Zero)
 
 spec :: Spec
 spec = describe "score de tennis" $ do
     it "score initial est 0-0" $ do
         scoreDeTennis initial `shouldBe` (Zero, Zero)
     it "score est 15-0 lorsque A marque" $
-        scoreDeTennis (aMarque) `shouldBe` (Quinze, Zero)
+        scoreDeTennis (aMarque initial) `shouldBe` (Quinze, Zero)
+    it "score est 0-15 lorsque B marque" $
+        scoreDeTennis (bMarque) `shouldBe` (Zero, Quinze)
+    it "score est 30-0 lorsque A marque 2 fois" $
+        scoreDeTennis (aMarque (aMarque initial)) `shouldBe` (Trente, Zero)
