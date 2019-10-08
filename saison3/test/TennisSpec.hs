e7825fa4819408c80e26cd9d2762f85303527fc5
Arnaud Bailly <arnaud.oqube@gmail.com>
2019-10-08 12:53:50 +0200

    introductino de la recursion du score
    Co-authored-by: dekristo <chris_yobs@hotmail.com>
    Co-authored-by: Mikaël Létang <mikael.letang@gmail.com>
    Co-authored-by: Denis <denis.jouenne@gmail.com>
    Co-authored-by: Thomas Benett <thomas@benett.io>

--- saison3/test/TennisSpec.hs
+++ saison3/test/TennisSpec.hs
@@ -3,11 +3,16 @@ module TennisSpec where
 import Test.Hspec
 
 data PartieDeTennis = Initial 
-                    | BMarque
+                    | BMarque { resteDeLaPartie :: PartieDeTennis }
                     | AMarque { resteDeLaPartie :: PartieDeTennis }
 
 data Point = Zero | Quinze | Trente
     deriving (Eq, Show)
+
+increment :: Point -> Point
+increment Zero = Quinze
+increment Quinze = Trente
+increment Trente = undefined
 
 initial :: PartieDeTennis
 initial = Initial
@@ -15,16 +20,19 @@ initial = Initial
 aMarque :: PartieDeTennis -> PartieDeTennis
 aMarque = AMarque 
 
-bMarque :: PartieDeTennis
+bMarque :: PartieDeTennis -> PartieDeTennis
 bMarque = BMarque
 
 type Score = (Point, Point)
 
 scoreDeTennis :: PartieDeTennis -> Score
-scoreDeTennis (AMarque (AMarque _)) = (Trente, Zero)
-scoreDeTennis (AMarque _)           = (Quinze, Zero)
-scoreDeTennis BMarque               = (Zero, Quinze)
-scoreDeTennis _                     = (Zero, Zero)
+scoreDeTennis (AMarque partie)      = 
+    let (scoreDeA, scoreDeB) = scoreDeTennis partie 
+    in (increment scoreDeA, scoreDeB)
+scoreDeTennis (BMarque partie)      = 
+    let (scoreDeA, scoreDeB) = scoreDeTennis partie 
+    in (scoreDeA, increment scoreDeB)
+scoreDeTennis Initial              = (Zero, Zero)
 
 spec :: Spec
 spec = describe "score de tennis" $ do
@@ -33,6 +41,10 @@ spec = describe "score de tennis" $ do
     it "score est 15-0 lorsque A marque" $
         scoreDeTennis (aMarque initial) `shouldBe` (Quinze, Zero)
     it "score est 0-15 lorsque B marque" $
-        scoreDeTennis (bMarque) `shouldBe` (Zero, Quinze)
+        scoreDeTennis (bMarque initial) `shouldBe` (Zero, Quinze)
     it "score est 30-0 lorsque A marque 2 fois" $
-        scoreDeTennis (aMarque (aMarque initial)) `shouldBe` (Trente, Zero)
+        scoreDeTennis (aMarque $ aMarque initial) `shouldBe` (Trente, Zero)
+    it "score est 0-30 lorsque B marque 2 fois" $
+        scoreDeTennis (bMarque $ bMarque initial) `shouldBe` (Zero, Trente)
+    it "score est 15-15 lorsque A et B marquent" $
+        scoreDeTennis (aMarque $ bMarque initial) `shouldBe` (Quinze, Quinze)
