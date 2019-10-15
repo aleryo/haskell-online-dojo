f15a005f13f9b07f25b73a96ec31ec0446c777e7
Arnaud Bailly <arnaud.oqube@gmail.com>
2019-10-15 11:08:15 +0200

    ajout des jeux

--- saison3/test/TennisSpec.hs
+++ saison3/test/TennisSpec.hs
@@ -6,45 +6,61 @@ data PartieDeTennis = Initial
                     | BMarque { resteDeLaPartie :: PartieDeTennis }
                     | AMarque { resteDeLaPartie :: PartieDeTennis }
 
-data Point = Zero | Quinze | Trente
+data Point = Zero | Quinze | Trente | Quarante | Avantage
     deriving (Eq, Show)
 
 increment :: Point -> Point
 increment Zero = Quinze
 increment Quinze = Trente
-increment Trente = undefined
+increment Trente = Quarante
 
-initial :: PartieDeTennis
-initial = Initial
+type Jeu = Int
 
-aMarque :: PartieDeTennis -> PartieDeTennis
-aMarque = AMarque 
-
-bMarque :: PartieDeTennis -> PartieDeTennis
-bMarque = BMarque
-
-type Score = (Point, Point)
+data Score = Score (Jeu, Jeu) (Point, Point) 
+    deriving (Eq, Show)
 
 scoreDeTennis :: PartieDeTennis -> Score
 scoreDeTennis (AMarque partie)      = 
-    let (scoreDeA, scoreDeB) = scoreDeTennis partie 
-    in (increment scoreDeA, scoreDeB)
+    case scoreDeTennis partie of
+        Score jeux (Quarante, Avantage) -> Score jeux (Quarante, Quarante)
+        Score jeux (Quarante, Quarante) -> Score jeux (Avantage, Quarante)
+        Score (jeuA, jeuB) (Quarante, _) -> Score (jeuA + 1, jeuB) (Zero, Zero)
+        Score jeux (scoreDeA, scoreDeB) -> Score jeux (increment scoreDeA, scoreDeB)
 scoreDeTennis (BMarque partie)      = 
-    let (scoreDeA, scoreDeB) = scoreDeTennis partie 
-    in (scoreDeA, increment scoreDeB)
-scoreDeTennis Initial              = (Zero, Zero)
+    case scoreDeTennis partie of
+        Score jeux (Avantage, Quarante) -> Score jeux (Quarante, Quarante) 
+        Score jeux (Quarante, Quarante) -> Score jeux (Quarante, Avantage)
+        Score (jeuA, jeuB) (_, Quarante) -> Score (jeuA, jeuB + 1) (Zero, Zero)
+        Score jeux (scoreDeA, scoreDeB) -> Score jeux (scoreDeA, increment scoreDeB)
+scoreDeTennis Initial              = Score (0, 0) (Zero, Zero)
 
 spec :: Spec
 spec = describe "score de tennis" $ do
     it "score initial est 0-0" $ do
-        scoreDeTennis initial `shouldBe` (Zero, Zero)
+        scoreDeTennis Initial `shouldBe` Score (0,0) (Zero, Zero)
     it "score est 15-0 lorsque A marque" $
-        scoreDeTennis (aMarque initial) `shouldBe` (Quinze, Zero)
+        scoreDeTennis (AMarque Initial) `shouldBe` Score (0, 0) (Quinze, Zero)
     it "score est 0-15 lorsque B marque" $
-        scoreDeTennis (bMarque initial) `shouldBe` (Zero, Quinze)
+        scoreDeTennis (BMarque Initial) `shouldBe` Score (0, 0) (Zero, Quinze)
     it "score est 30-0 lorsque A marque 2 fois" $
-        scoreDeTennis (aMarque $ aMarque initial) `shouldBe` (Trente, Zero)
+        scoreDeTennis (AMarque $ AMarque Initial) `shouldBe` Score (0, 0) (Trente, Zero)
     it "score est 0-30 lorsque B marque 2 fois" $
-        scoreDeTennis (bMarque $ bMarque initial) `shouldBe` (Zero, Trente)
+        scoreDeTennis (BMarque $ BMarque Initial) `shouldBe` Score (0, 0) (Zero, Trente)
     it "score est 15-15 lorsque A et B marquent" $
-        scoreDeTennis (aMarque $ bMarque initial) `shouldBe` (Quinze, Quinze)
+        scoreDeTennis (AMarque $ BMarque Initial) `shouldBe` Score (0, 0) (Quinze, Quinze)
+    it "score est 40-40 lorsque A et B marquent chacun 3 fois" $
+        scoreDeTennis (AMarque $ AMarque $ AMarque $ BMarque $ BMarque $ BMarque Initial)
+            `shouldBe` Score (0, 0) (Quarante, Quarante)
+    it "Si le score est de 40-40 lorsque A marque il prend l'avantage" $
+        scoreDeTennis (AMarque $ AMarque $ AMarque $ AMarque $ BMarque $ BMarque $ BMarque Initial)
+            `shouldBe` Score (0, 0) (Avantage, Quarante)
+    it "Si A à l'avantage et que B marque le score est de 40-40" $
+        scoreDeTennis (BMarque $ AMarque $ AMarque $ AMarque $ AMarque $ BMarque $ BMarque $ BMarque Initial)
+            `shouldBe` Score (0, 0) (Quarante, Quarante)
+    it "Si B à l'avantage et que A marque le score est de 40-40" $
+        scoreDeTennis (AMarque $ BMarque $ AMarque $ AMarque $ AMarque $ BMarque $ BMarque $ BMarque Initial)
+            `shouldBe` Score (0, 0) (Quarante, Quarante)
+    it "score est 1-0 sur le jeu précédent et 0-0 sur le jeu suivant lorsque A marque 4 fois" $
+        scoreDeTennis (AMarque $ AMarque $ AMarque $ AMarque Initial) `shouldBe` Score (1, 0) (Zero, Zero)
+    it "score est 0-1 sur le jeu précédent et 0-0 sur le jeu suivant lorsque B marque 4 fois" $
+        scoreDeTennis (BMarque $ BMarque $ BMarque $ BMarque Initial) `shouldBe` Score (0, 1) (Zero, Zero)
