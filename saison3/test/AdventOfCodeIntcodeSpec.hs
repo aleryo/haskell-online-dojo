69f49cd4004132e06bbebecdf2da0cce3cc4797e
Fabien Lamarque <fabien.lamarque@gmail.com>
2020-01-28 11:38:50 +0100

    fixed end of part 1

--- saison3/test/AdventOfCodeIntcodeSpec.hs
+++ saison3/test/AdventOfCodeIntcodeSpec.hs
@@ -2,6 +2,7 @@ module AdventOfCodeIntcodeSpec where
 
 import Test.Hspec
 import Data.List.Split
+import Data.Array
 
 newtype Cursor = Cursor Int
 
@@ -19,9 +20,12 @@ start startingMemory = Interpreter (startingMemory, Cursor 0)
 interpret :: [Int] -> [Int]
 interpret = memory . interpret' . start
 
-interpretReprogrammed :: [Int] -> Integer -> Integer -> [Int]
----nterpretReprogrammed interpreter  _ _  = memory . interpret' . start $ interpreter
-interpretReprogrammed interpreter  a b  = memory . interpret' . (put (put (start interpreter) 1 12 ) 2 23)
+interpretReprogrammed :: [Int] -> Int -> Int -> [Int]
+interpretReprogrammed interpreter  a b  = memory  (interpret'  ((put (put (start interpreter) 1 a ) 2 b)))
+
+output :: [Int] -> Int -> Int -> Int
+output interpreter a b  = head  (interpretReprogrammed interpreter  a b)
+
 
 currentInstruction :: Interpreter -> Int
 currentInstruction interpreter = (memory interpreter)!!(cursor interpreter)
@@ -93,8 +97,9 @@ spec = describe "Intcode Computer - Day 2" $ do
 
     it "can reprogram our program" $ do
         let input =  parse "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0"
-        interpretReprogrammed input 12 0 `shouldBe` [337024,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,0,1,5,19,1,2,6,23,2,1,27,5,3,2,9,31,9,1,5,35,10,2,6,39,20,2,6,43,40,1,5,47,41,2,9,51,123,1,5,55,124,1,10,59,128,1,63,6,130,1,9,67,133,1,71,6,135,1,75,13,140,2,79,13,700,2,9,83,2100,1,87,5,2101,1,9,91,2104,2,10,95,8416,1,5,99,8417,1,103,9,8420,1,13,107,8425,2,111,10,33700,1,115,5,33701,2,13,119,168505,1,9,123,168508,1,5,127,168509,2,131,6,337018,1,135,5,337019,1,139,6,337021,1,143,6,337023,1,2,147,337023,1,151,5,0,99,2,14,0,0]
+        interpretReprogrammed input 12 2 `shouldBe` [4484226,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,24,1,5,19,25,2,6,23,50,1,27,5,51,2,9,31,153,1,5,35,154,2,6,39,308,2,6,43,616,1,5,47,617,2,9,51,1851,1,5,55,1852,1,10,59,1856,1,63,6,1858,1,9,67,1861,1,71,6,1863,1,75,13,1868,2,79,13,9340,2,9,83,28020,1,87,5,28021,1,9,91,28024,2,10,95,112096,1,5,99,112097,1,103,9,112100,1,13,107,112105,2,111,10,448420,1,115,5,448421,2,13,119,2242105,1,9,123,2242108,1,5,127,2242109,2,131,6,4484218,1,135,5,4484219,1,139,6,4484221,1,143,6,4484223,1,2,147,4484225,1,151,5,0,99,2,14,0,0]
 
+    
 
     it "99 ends the program" $ do 
         interpret  [99] `shouldBe` [99]
