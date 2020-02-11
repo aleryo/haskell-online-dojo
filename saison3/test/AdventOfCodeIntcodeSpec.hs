db96350aa8338646354aba0ac7e9177c962b2d63
Fabien Lamarque <fabien.lamarque@gmail.com>
2020-02-11 13:32:20 +0100

    11 février, Christophe Cadillac, Sébastien Rabiller, Anna Severin, Arnaud Bailly, Anael Chardan, Bec, Fabien

--- saison3/test/AdventOfCodeIntcodeSpec.hs
+++ saison3/test/AdventOfCodeIntcodeSpec.hs
@@ -3,10 +3,14 @@ module AdventOfCodeIntcodeSpec where
 import Test.Hspec
 import Data.List.Split
 import Data.Array
+import Data.Function ((&))
 
 newtype Cursor = Cursor Int
 
 newtype Interpreter = Interpreter ([Int], Cursor)
+
+newtype Setup = Setup (Int, Int)
+
 
 memory :: Interpreter -> [Int]
 memory (Interpreter (memory, _)) = memory
@@ -21,10 +25,10 @@ interpret :: [Int] -> [Int]
 interpret = memory . interpret' . start
 
 interpretReprogrammed :: [Int] -> Int -> Int -> [Int]
-interpretReprogrammed interpreter  a b  = memory  (interpret'  ((put (put (start interpreter) 1 a ) 2 b)))
+interpretReprogrammed interpreter a b  = memory  $ interpret'  $ (put (put (start interpreter) 1 a ) 2 b)
 
 output :: [Int] -> Int -> Int -> Int
-output interpreter a b  = head  (interpretReprogrammed interpreter  a b)
+output interpreter a b  = head  $ interpretReprogrammed interpreter a b
 
 
 currentInstruction :: Interpreter -> Int
@@ -75,6 +79,14 @@ parse input = convert $ (splitOn "," input)
 convert :: [String] -> [Int]
 convert = map read
 
+
+findParameters :: [Int] -> Int -> ( Int, Int)
+findParameters input result =
+    [(head (interpretReprogrammed input a b), (a, b)) | a <- [0..99], b <- [0..99]]
+    & filter (\(actualResult, _) -> actualResult == result)
+    & head
+    & snd
+
 spec :: Spec
 spec = describe "Intcode Computer - Day 2" $ do
     -- 1,0,0,0,99 becomes 2,0,0,0,99 
@@ -98,8 +110,12 @@ spec = describe "Intcode Computer - Day 2" $ do
     it "can reprogram our program" $ do
         let input =  parse "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0"
         interpretReprogrammed input 12 2 `shouldBe` [4484226,12,2,2,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,24,1,5,19,25,2,6,23,50,1,27,5,51,2,9,31,153,1,5,35,154,2,6,39,308,2,6,43,616,1,5,47,617,2,9,51,1851,1,5,55,1852,1,10,59,1856,1,63,6,1858,1,9,67,1861,1,71,6,1863,1,75,13,1868,2,79,13,9340,2,9,83,28020,1,87,5,28021,1,9,91,28024,2,10,95,112096,1,5,99,112097,1,103,9,112100,1,13,107,112105,2,111,10,448420,1,115,5,448421,2,13,119,2242105,1,9,123,2242108,1,5,127,2242109,2,131,6,4484218,1,135,5,4484219,1,139,6,4484221,1,143,6,4484223,1,2,147,4484225,1,151,5,0,99,2,14,0,0]
-
+   
+    it "can find input parameters for result 19690720" $ do
+        let input =  parse "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,5,19,23,2,6,23,27,1,27,5,31,2,9,31,35,1,5,35,39,2,6,39,43,2,6,43,47,1,5,47,51,2,9,51,55,1,5,55,59,1,10,59,63,1,63,6,67,1,9,67,71,1,71,6,75,1,75,13,79,2,79,13,83,2,9,83,87,1,87,5,91,1,9,91,95,2,10,95,99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5,119,2,13,119,123,1,9,123,127,1,5,127,131,2,131,6,135,1,135,5,139,1,139,6,143,1,143,6,147,1,2,147,151,1,151,5,0,99,2,14,0,0"
+        findParameters input 19690720 `shouldBe` (56, 96) 
     
+
 
     it "99 ends the program" $ do 
         interpret  [99] `shouldBe` [99]
